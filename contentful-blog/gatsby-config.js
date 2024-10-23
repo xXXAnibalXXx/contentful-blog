b3b4555036aac1985bac0058d17a428612b0121f
xXXAnibalXXx <cpp.hmuhammad@gmail.com>
2024-10-23 16:28:36 -0400

    Initial commit

---
+++ contentful-blog/gatsby-config.js
@@ -0,0 +1,48 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // Add Contentful Plugin here
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `w5qy0o39t024`, // Replace with your actual Contentful Space ID
+        accessToken: `r_2NsgqDRXyGRYJ8n68ct32og8MTM5G3CUHXcS5ufFA`, // Replace with your actual Content Delivery API key
+      },
+    },
+  ],
+}
